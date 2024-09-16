104997e4262561a78f2cb67c7b38abe3dbb238fd
Kais Riahi <ka1s@ka1s.dev>
2024-09-17 00:24:09 +0200

    Initialisation du Projet

---
+++ src/main/java/com/wemanity/kata_dictionary_replacer/KataDictionaryReplacerApplication.java
@@ -0,0 +1,13 @@
+package com.wemanity.kata_dictionary_replacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataDictionaryReplacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataDictionaryReplacerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wemanity/kata_dictionary_replacer/KataDictionaryReplacerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemanity.kata_dictionary_replacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataDictionaryReplacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
